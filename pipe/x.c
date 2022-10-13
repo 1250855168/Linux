791825f4edfd41aa3ae8d2fd020516a7e92b2a23
xxx <1250855168@qq.com>
2022-10-13 15:51:33 +0800

    updata

---
+++ pipe/x.c
@@ -0,0 +1,36 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+int main()
+{
+    int fd;
+    pid_t pid;
+    char buf[1024];
+    pid = fork();
+
+    while (1)
+    {
+        if (pid == 0)
+        {
+            fd = open("B", O_RDWR);
+            printf("x:");
+            fgets(buf, 1024, stdin);
+            write(fd, buf, 1024);
+        }
+
+        if (pid > 0)
+        {
+            fd = open("A", O_RDWR);
+            read(fd, buf, 1024);
+            printf("xx:%s", buf);
+        }
+    }
+
+    return 0;
+}
---
+++ pipe/xx.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+int main()
+{
+    int fd_pipe[2];
+    int fd;
+    int ret;
+    pid_t pid;
+    char buf[1024];
+    pid = fork();
+
+    while (1)
+    {
+        if (pid == 0)
+        {
+            fd = open("A", O_RDWR);
+            printf("xx:");
+            fgets(buf, 1024, stdin);
+            write(fd, buf, 1024);
+        }
+
+        if (pid > 0)
+        {
+            fd = open("B", O_RDWR);
+            read(fd, buf, 1024);
+            printf("x:%s", buf);
+        }
+    }
+
+    return 0;
+}
